c77b445e87fdb3a7c09a1fd9dcf33123dadb3976
zym-starx <110522703+zym-starx@users.noreply.github.com>
2022-09-01 14:37:40 +0300

    feature for deleting old soundfiles to not get cramped up

--- main_try2.py
+++ main_try2.py
@@ -155,7 +155,7 @@ def main():
 
     elif(mode == "TTS"):
         Speaking(root)
-        root.after(500, TTS)
+        root.after(100, TTS)
         Standby(root)
 
         if (exmode == "OP"):
@@ -174,3 +174,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+import os, shutil
+folder = 'C:/zym/nora/nora_v1.1.0/testsounds'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
